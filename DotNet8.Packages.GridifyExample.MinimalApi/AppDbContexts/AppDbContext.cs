ec383f96ccbc04c8dfd6913bbf48793056336e38
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-24 18:57:48 +0630

    Format Code

--- DotNet8.Packages.GridifyExample.MinimalApi/AppDbContexts/AppDbContext.cs
+++ DotNet8.Packages.GridifyExample.MinimalApi/AppDbContexts/AppDbContext.cs
@@ -1,13 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
-namespace DotNet8.Packages.GridifyExample.MinimalApi.AppDbContexts
-{
-    public class AppDbContext : DbContext
-    {
-        public AppDbContext(DbContextOptions options) : base(options)
-        {
-        }
+namespace DotNet8.Packages.GridifyExample.MinimalApi.AppDbContexts;
 
-        public DbSet<Tbl_Blog> Tbl_Blogs { get; set; }
-    }
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options)
+        : base(options) { }
+
+    public DbSet<Tbl_Blog> Tbl_Blogs { get; set; }
 }
